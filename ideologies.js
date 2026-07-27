30041c1413f6550136bba4727406e8fa83615d11
Araxiel <fireops@gmail.com>
2026-07-27 02:39:09 -0400

    reintroduces 8values inspired result screen, adds experimental icons, other small fixes

--- ideologies.js
+++ ideologies.js
@@ -1,22 +1,22 @@
 const ideologies = [
     {
         "name": "Federal Commonwealth",
-        "stats": { "econ": 75, "dipl": 65, "govt": 25, "synd": 55, "ordr": 80 }
+        "stats": { "econ": 75, "dipl": 35, "govt": 75, "synd": 55, "ordr": 80 }
     },
     {
         "name": "Frontier Autonomism",
-        "stats": { "econ": 45, "dipl": 75, "govt": 85, "synd": 40, "ordr": 75 }
+        "stats": { "econ": 45, "dipl": 25, "govt": 15, "synd": 40, "ordr": 75 }
     },
     {
         "name": "Syndic Coalition",
-        "stats": { "econ": 85, "dipl": 65, "govt": 55, "synd": 90, "ordr": 90 }
+        "stats": { "econ": 85, "dipl": 35, "govt": 45, "synd": 90, "ordr": 90 }
     },
     {
         "name": "Corporate Directorate",
-        "stats": { "econ": 15, "dipl": 35, "govt": 30, "synd": 10, "ordr": 60 }
+        "stats": { "econ": 15, "dipl": 65, "govt": 60, "synd": 10, "ordr": 60 }
     },
     {
         "name": "Dominion Traditionalism",
-        "stats": { "econ": 25, "dipl": 15, "govt": 20, "synd": 10, "ordr": 10 }
+        "stats": { "econ": 25, "dipl": 85, "govt": 80, "synd": 10, "ordr": 10 }
     }
 ]
